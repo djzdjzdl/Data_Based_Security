4d55ae666e08aff2958d036870d9e0120e6a4bfe
djzdjzdl <a272714@naver.com>
2021-12-02 05:42:06 +0900

    [Update] EDA Code updated
    Update info
    - Saving PE feature function is run as independent
    - Validation / Test file path is added

--- EDA.py
+++ EDA.py
@@ -27,13 +27,15 @@ class Save_Pe_Feature:
     ]
 
     #Train data based data analysis
-    file_path = './train_data'
+    #file_path = './train_data'
+    file_path = './validation_data'
+    #file_path = './test_data'
     NULL_ROW = [0 for x in COLS]
 
-    def __init__(self):
+    def __init__(self, filename, label):
         print("[+] Saving PE File Features")
         files = os.listdir(Save_Pe_Feature.file_path)
-        with open('./pe_features.csv', 'w+', newline='') as csvs:
+        with open(filename, 'w+', newline='') as csvs:
             wp = csv.writer(csvs)
             wp.writerow(Save_Pe_Feature.COLS)
             for idx, file in enumerate(files):
@@ -41,12 +43,13 @@ class Save_Pe_Feature:
                     pe = pefile.PE(os.path.join(Save_Pe_Feature.file_path, file) )
                     feature = Save_Pe_Feature.Extract_Pe_Features(pe, file)
                 except:
-                    print('[+] {}'.format(file))
+                    print('[+] {} with NULL'.format(file))
                     feature = Save_Pe_Feature.NULL_ROW
                     feature[0] = file
                 wp.writerow(feature)
         print("[-] Saving PE File Features - Done")
-        Save_Pe_Feature.Save_Pe_Label()
+        if 'test' not in filename:
+            Save_Pe_Feature.Save_Pe_Label(filename, label)
 
     #Extract from PE Features 
     def Extract_Pe_Features(pe, filename):
@@ -226,24 +229,51 @@ class Save_Pe_Feature:
 
         return row
 
-    def Save_Pe_Label():
+    def Save_Pe_Label(filename, label):
         print("[+] Adding Labels")
-        with open('./train_label.csv', 'r') as f:
-            with open('./pe_features.csv', 'r') as ff:
-                with open('./new_pe_features.csv', 'w+', newline='') as fff:
-                    cr = csv.reader(f)
-                    crr = csv.reader(ff)
-                    cw = csv.writer(fff)
-                    for line in crr:
-                        f.seek(0)
-                        for line2 in cr:
-                            if line[0] == line2[0]:
-                                cw.writerow(line + line2[1:])
-                                break
-        os.remove('./pe_features.csv')
-        shutil.move('./new_pe_features.csv','./pe_features.csv')
+        if label is not '0':
+            with open(label, 'r') as f:
+                with open(filename, 'r') as ff:
+                    with open('./new_pe_features.csv', 'w+', newline='') as fff:
+                        cr = csv.reader(f)
+                        crr = csv.reader(ff)
+                        cw = csv.writer(fff)
+                        for line in crr:
+                            f.seek(0)
+                            for line2 in cr:
+                                if line[0] == line2[0]:
+                                    cw.writerow(line + line2[1:])
+                                    break
+            os.remove(filename)
+            shutil.move('./new_pe_features.csv',filename)
+        else:
+            print("[+] This is Test file")
         print("[-] Adding Labels - Done")
 
 if __name__ == "__main__":
+    '''
+    For Train Set
+    './train_data'
+    './pe_features.csv'
+    './train_label.csv'
+    '''
+    #Save_Pe_Feature('./pe_features.csv', 'train_label.csv')
+
+    '''
+    For Validation Set
+    './validation_data'
+    './valiation_features.csv'
+    './valiation_label.csv'
+    '''
+    #Save_Pe_Feature('./validation_features.csv', 'validation_label.csv')
+
+    '''
+    For Test Set
+    './test_data'
+    './test_features.csv'
+    '''
+    Save_Pe_Feature('./test_features.csv', '0')
+
     #Making Static PE features
-    Save_Pe_Feature()
+    
+    
